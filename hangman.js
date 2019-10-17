814ab46733709b3211da9859a62b13c63ca931bf
Moshe Stern <moshecstern@gmail.com>
2019-10-17 17:37:40 -0400

    logic completed

--- hangman.js
+++ hangman.js
@@ -1,7 +1,7 @@
 
 // make list of variables for the ids starting point
 var villans = ["joker", "catwoman", "penguin",];
-var wordblanks= [];
+var wordblanks = [];
 // variables for score
 var wins = 0;
 var losses = 0;
@@ -13,60 +13,71 @@ var villainLetters = document.getElementById("villainLetters");
 var userGuess = document.getElementById("userGuess");
 var wins = document.getElementById("wins");
 var losses = document.getElementById("losses");
-var turns = document.getElementById("turns");
-
+// var turns = document.getElementById("turns");
+// turns = turnsCount;
+// functions 
+// function updateStats(){
+//     wins.textContent = wins;
+//     losses.textContent = losses;
+//     turns.textContent = turns;
+// }
+  // Function that updates the turn...
+//   function updateScore() {
+//     document.querySelector("turns").innerHTML = turnsCount;
+//   }
 // Randomly chooses a choice from the options array. This is the Computer's guess.
 var computerGuess = villans[Math.floor(Math.random() * villans.length)];
 console.log("This is the computuer guess: " + computerGuess);
+
+function displayLettersToGuess(wordToGuess) {
+    // villain.innerText = '_ '.repeat(wordToGuess.length);
+// for loop that pushes letter (if correct) into array that displays wordblanks into villains
+    for (var i = 0; i < wordToGuess.length; i++) {
+        console.log(wordToGuess[i]);
+        wordblanks.push("_");
+    }
+    document.getElementById("villains").textContent = wordblanks.join(" ");
+}
 displayLettersToGuess(computerGuess);
 
-// function displayLettersToGuess(wordToGuess) {
-//     villain.innerText = '_ '.repeat(wordToGuess.length);
-// }
-
-for (var i= 0; i < villans.length; i++){
-    console.log(villan[i]);
-    wordblanks.push("_ ");
-    document.getElementById("villain").textContent = wordblanks.join(" ");
-}
-// change to for loop
-
-// creating function when key is pressed
-// document.addEventListener("keyup", function(event) {
-//     console.log(event);
-//     // Determines which key was pressed.
-//       var userGuess = event.key;
-//       guesses.push(userGuess);
-//     console.log(userGuess);
-
-//     }) 
 document.onkeyup = function (event) {
     var userGuess = event.key;
     console.log("This is the user guess: " + userGuess);
-    guesses.push(userGuess);
-    console.log(userGuess);
+    // pushes userGuess (which is now event.key) in array of wordblanks
+    wordblanks.push(userGuess);
+    console.log("This is also the user guess" + userGuess);
     // check to see if user guess's letteers match cpus guess
     for (var i = 0; i < computerGuess.length; i++) {
         // console.log(computerGuess[i]);
+        // var userGuess = event.key.toLowerCase();
         if (userGuess === computerGuess[i]) {
             console.log("letter is found");
-            // villain.innerText = userGuess + '_ ';
-            //  top line here is changing whole var of userguess. need to update letter only
+            wordblanks[i] = userGuess;
             // add user letter to the word; take away the userscore and add the letter
             console.log(computerGuess[i] + " and " + userGuess)
             // document.textContent.getElementById(userGuess)= userGuess[i];
-            
-        } else {
+            console.log(wordblanks);
+            document.getElementById("villains").textContent = wordblanks.join(" ");
+        } } if (userGuess !== computerGuess) {
             console.log("no matches");
             //detract points from total guesses
+             guesses--
             console.log(turns--);
-            turns--; 
+            // make turn count go down by 1
+            turns--;
+            console.log("this is how many turns i have left " + turns);
+            // updateStats();
+            // turnsCount.innerHTML = turnsCount;
+            // document.getElementById("turns").textContent = turns;
             // add to letters guessed
+            // guesses.push(userGuess);
+            // console.log(guesses);
         }
-    // if then statement only running entire code if turns > 0
+        // if then statement only running entire code if turns > 0
+
+        // userGuess.textContent = "your guesses " + guesses;
+        // wins.textContent = wins;
+        // losses.textContent = losses;
+        // turns.textContent = turns;
     
-    userGuess.textContent = "your guesses " + guesses;
-    wins.textContent = "Wins: " + wins;
-    losses.textContent = "Losses: " + losses;
-    turns.textContent = "Turns: " + turns;
-}};
+};
